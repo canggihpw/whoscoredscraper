64603e2f031346d0aefd179c07d77837b6b98255
canggihpw <cangcappo@gmail.com>
2017-08-12 08:12:01 +0700

    tidy up comments

--- WSTournamentList.py
+++ WSTournamentList.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 11 13:52:12 2017
-
-@author: canggih
+WSTournamentList.py
+A list of name and URL of tournaments
+Used to map between ID and the URL
+---
 """
 
-TournamentLists = [[0,u'Premier League', u'https://www.whoscored.com/Regions/252/Tournaments/2/England-Premier-League'], 
-                   [1,u'Serie A', u'https://www.whoscored.com/Regions/108/Tournaments/5/Italy-Serie-A'], 
-[2,u'La Liga', u'https://www.whoscored.com/Regions/206/Tournaments/4/Spain-La-Liga'], 
-[3,u'Bundesliga', u'https://www.whoscored.com/Regions/81/Tournaments/3/Germany-Bundesliga'], 
-[4,u'Ligue 1', u'https://www.whoscored.com/Regions/74/Tournaments/22/France-Ligue-1'], 
-[5,u'Liga NOS', u'https://www.whoscored.com/Regions/177/Tournaments/21/Portugal-Liga-NOS'], 
-[6,u'Eredivisie', u'https://www.whoscored.com/Regions/155/Tournaments/13/Netherlands-Eredivisie'], 
-[7,u'Premier League', u'https://www.whoscored.com/Regions/182/Tournaments/77/Russia-Premier-League'], 
-[8,u'Brasileir\xe3o', u'https://www.whoscored.com/Regions/31/Tournaments/95/Brazil-Brasileir%C3%A3o'], 
-[9,u'Major League Soccer', u'https://www.whoscored.com/Regions/233/Tournaments/85/USA-Major-League-Soccer'], 
-[10,u'Super Lig', u'https://www.whoscored.com/Regions/225/Tournaments/17/Turkey-Super-Lig'], 
-[11,u'Championship', u'https://www.whoscored.com/Regions/252/Tournaments/7/England-Championship'], 
-[12,u'Primera Divisi\xf3n', u'https://www.whoscored.com/Regions/11/Tournaments/68/Argentina-Primera-Divisi%C3%B3n'], 
-[13,u'Super league', u'https://www.whoscored.com/Regions/45/Tournaments/162/China-Super-league'], 
-[14,u'Eliteserien', u'https://www.whoscored.com/Regions/165/Tournaments/41/Norway-Eliteserien'], 
-[15,u'Allsvenskan', u'https://www.whoscored.com/Regions/212/Tournaments/40/Sweden-Allsvenskan'], 
-[16,u'Bundesliga II', u'https://www.whoscored.com/Regions/81/Tournaments/6/Germany-Bundesliga-II'], 
-[17,u'UEFA Champions League', u'https://www.whoscored.com/Regions/250/Tournaments/12/Europe-UEFA-Champions-League'], 
+TournamentLists = [[0,u'Premier League', u'https://www.whoscored.com/Regions/252/Tournaments/2/England-Premier-League'],
+                   [1,u'Serie A', u'https://www.whoscored.com/Regions/108/Tournaments/5/Italy-Serie-A'],
+[2,u'La Liga', u'https://www.whoscored.com/Regions/206/Tournaments/4/Spain-La-Liga'],
+[3,u'Bundesliga', u'https://www.whoscored.com/Regions/81/Tournaments/3/Germany-Bundesliga'],
+[4,u'Ligue 1', u'https://www.whoscored.com/Regions/74/Tournaments/22/France-Ligue-1'],
+[5,u'Liga NOS', u'https://www.whoscored.com/Regions/177/Tournaments/21/Portugal-Liga-NOS'],
+[6,u'Eredivisie', u'https://www.whoscored.com/Regions/155/Tournaments/13/Netherlands-Eredivisie'],
+[7,u'Premier League', u'https://www.whoscored.com/Regions/182/Tournaments/77/Russia-Premier-League'],
+[8,u'Brasileir\xe3o', u'https://www.whoscored.com/Regions/31/Tournaments/95/Brazil-Brasileir%C3%A3o'],
+[9,u'Major League Soccer', u'https://www.whoscored.com/Regions/233/Tournaments/85/USA-Major-League-Soccer'],
+[10,u'Super Lig', u'https://www.whoscored.com/Regions/225/Tournaments/17/Turkey-Super-Lig'],
+[11,u'Championship', u'https://www.whoscored.com/Regions/252/Tournaments/7/England-Championship'],
+[12,u'Primera Divisi\xf3n', u'https://www.whoscored.com/Regions/11/Tournaments/68/Argentina-Primera-Divisi%C3%B3n'],
+[13,u'Super league', u'https://www.whoscored.com/Regions/45/Tournaments/162/China-Super-league'],
+[14,u'Eliteserien', u'https://www.whoscored.com/Regions/165/Tournaments/41/Norway-Eliteserien'],
+[15,u'Allsvenskan', u'https://www.whoscored.com/Regions/212/Tournaments/40/Sweden-Allsvenskan'],
+[16,u'Bundesliga II', u'https://www.whoscored.com/Regions/81/Tournaments/6/Germany-Bundesliga-II'],
+[17,u'UEFA Champions League', u'https://www.whoscored.com/Regions/250/Tournaments/12/Europe-UEFA-Champions-League'],
 [18,u'UEFA Europa League', u'https://www.whoscored.com/Regions/250/Tournaments/30/Europe-UEFA-Europa-League']]
